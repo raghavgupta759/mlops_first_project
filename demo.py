5af5b0cf0770ffd0f738afc3f2636d9eaf3719d4
check <rgupta70610@gmail.com>
2026-06-24 14:50:54 +0530

    2 day final

--- demo.py
+++ demo.py
@@ -11,9 +11,12 @@
 from src.logger import logging
 from src.exception import MyException
 import sys
+from dotenv import load_dotenv
+load_dotenv()
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
